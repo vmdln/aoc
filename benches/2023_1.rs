4f42e2ae59107e67004a605731efb4641ebd0e8c
vmdln <110718729+vmdln@users.noreply.github.com>
2023-12-01 21:47:26 +0100

    Rename assets

--- benches/2023_1.rs
+++ benches/2023_1.rs
@@ -1,3 +1,3 @@
 use aoc::y2023::d1::{Part1, Part2};
 
-aoc::bench!("../assets/2023/big_1.txt", Part1, Part2);
+aoc::bench!("../assets/2023/01_big.txt", Part1, Part2);
--- src/bin/2023_1.rs
+++ src/bin/2023_1.rs
@@ -1,3 +1,3 @@
 use aoc::y2023::d1::{Part1, Part2};
 
-aoc::solve_main!("../../assets/2023/1.txt", Part1, Part2);
+aoc::solve_main!("../../assets/2023/01.txt", Part1, Part2);
