477356887d3402befdbecb6a5423d8e9c764ceee
vmdln <110718729+vmdln@users.noreply.github.com>
2024-02-03 13:07:19 +0100

    Warn on `clippy::pedantic` globally

--- src/bin/2015_2.rs
+++ src/bin/2015_2.rs
@@ -1,5 +1,3 @@
-#![warn(clippy::pedantic)]
-
 use std::str::FromStr;
 
 use anyhow::{anyhow, Error, Result};
--- src/bin/2015_3.rs
+++ src/bin/2015_3.rs
@@ -1,5 +1,3 @@
-#![warn(clippy::pedantic)]
-
 use std::collections::HashSet;
 
 use tap::prelude::*;
